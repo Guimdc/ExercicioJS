5e398f8851669c0185af4589f5561569db0fdec2
netó! <82977733+neto-luciano@users.noreply.github.com>
2022-10-17 23:11:30 -0300

    Delete exercicio1 directory

--- exercicio1/js/script.js
+++
@@ -1,5 +0,0 @@
-document.querySelector("#button").onclick = function (){
-    let numResult = Number (document.querySelector("#num").value**2);
-    document.querySelector("#numResult").value = numResult;
-    return false;
-}
